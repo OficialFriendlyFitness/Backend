401cb5e286939b592a21af75cb1f654a8c681226
LuisRz1 <langulor1@upao.edu.pe>
2023-10-14 01:52:57 -0500

    first-commit

---
+++ src/main/java/edu/pe/upao/friendlyfitnessbackend/FriendlyFitnessBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.pe.upao.friendlyfitnessbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendlyFitnessBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendlyFitnessBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/pe/upao/friendlyfitnessbackend/FriendlyFitnessBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pe.upao.friendlyfitnessbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendlyFitnessBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
